2ff84b76fa1a3849a7e85cab6c9645acfdfb99b7
alexgirardello <alexandre.girardello@estudante.ifro.edu.br>
2022-11-28 21:37:56 -0400

    initial

---
+++ model/models.py
@@ -0,0 +1,34 @@
+import peewee
+import mysql.connector
+
+
+class BaseModel(peewee.Model):
+
+    def __init__(self, *args, **kwargs):
+        try:
+            self.create_table()
+        except peewee.OperationalError as erro:
+            print(erro)
+
+        super().__init__(*args, **kwargs)
+
+    class Meta:
+        """
+        Conecta no servidor MySQL
+        Cria o banco de dados, caso não exista
+        """
+        name = "cbbd"
+        user = "root"
+        password = ""
+        host = "127.0.0.1"
+
+        database = mysql.connector.connect(
+            user=user,
+            password=password,
+            host=host,
+        )
+
+        cursor = database.cursor()
+        cursor.execute("SHOW DATABASES")
+        databases = [database[0] for database in cursor]
+        cursor.execute(f"CREATE DATABASE {name}") if name not in databases else False
