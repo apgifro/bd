944a802bf1cb1e8327ad1861ddaffc21c340dce0
alexgirardello <alexandre.girardello@estudante.ifro.edu.br>
2022-12-04 02:12:11 -0400

    16 model

--- model/models.py
+++ model/models.py
@@ -3,6 +3,10 @@ import mysql.connector
 
 
 class BaseModel(peewee.Model):
+    """
+    Classe básica para criação de novas
+    classes model a partir do peewee
+    """
 
     def __init__(self, *args, **kwargs):
         try:
@@ -33,3 +37,67 @@ class BaseModel(peewee.Model):
         databases = [database[0] for database in cursor]
         cursor.execute(f"CREATE DATABASE {name}") if name not in databases else False
         cursor.execute(f"USE {name}")
+
+
+class Especialidade(BaseModel):
+    pass
+
+
+class Contato(BaseModel):
+    pass
+
+
+class Revisor(BaseModel):
+    pass
+
+
+class Artigo_Revisor(BaseModel):
+    pass
+
+
+class Artigo(BaseModel):
+    pass
+
+
+class Palavras_Chave(BaseModel):
+    pass
+
+
+class Participante(BaseModel):
+    pass
+
+
+class Autor(BaseModel):
+    pass
+
+
+class Inscrito(BaseModel):
+    pass
+
+
+class Cientista(BaseModel):
+    pass
+
+
+class Local(BaseModel):
+    pass
+
+
+class Local_Atividade(BaseModel):
+    pass
+
+
+class Atividade(BaseModel):
+    pass
+
+
+class Sessao_Tecnica(BaseModel):
+    pass
+
+
+class Palestra(BaseModel):
+    pass
+
+
+class Minicurso(BaseModel):
+    pass
