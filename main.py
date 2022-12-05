6d2f15aa8625c86877e0485c1ee6512dcf73fccb
alexgirardello <alexandre.girardello@estudante.ifro.edu.br>
2022-12-05 14:31:40 -0400

    toolbar, new article & corrector screens

--- main.py
+++ main.py
@@ -1,26 +1,42 @@
 from kivy.lang import Builder
-from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.screenmanager import ScreenManager, Screen, SlideTransition
 from kivymd.app import MDApp
+from kivymd.uix.floatlayout import MDFloatLayout
+from kivymd.uix.tab import MDTabsBase
+
+
+class Tab(MDFloatLayout, MDTabsBase):
+    pass
 
 
 class MainScreen(Screen):
 
-    Builder.load_file("view/main.kv")
+    def __init__(self, **kw):
+        self.data = {
+            'Revisor': ["account", "on_press", self.corrector],
+            'Artigo': ["book-open-page-variant-outline", "on_press", self.article]
+        }
+        Builder.load_file("view/main.kv")
+        super().__init__(**kw)
 
-    data = {
-        'Artigo': ['book-open-page-variant-outline', "on_press", lambda x: print("Artigo")],
-        'Revisor': 'account',
-    }
+    def on_enter(self):
+        print(self.manager.get_screen("main").ids)
+
+    def corrector(self, button):
+        self.manager.corrector()
+
+    def article(self, button):
+        self.manager.article()
 
 
-class NewScreen(Screen):
+class CorrectorScreen(Screen):
 
-    Builder.load_file("view/new.kv")
+    Builder.load_file("view/corrector.kv")
 
 
-class EditScreen(Screen):
+class ArticleScreen(Screen):
 
-    Builder.load_file("view/edit.kv")
+    Builder.load_file("view/article.kv")
 
 
 class ScreenManagement(ScreenManager):
@@ -28,22 +44,40 @@ class ScreenManagement(ScreenManager):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-    def main_screen(self):
+    def main(self):
         self.current = "main"
 
-    def new_screen(self):
-        self.current = "new"
+    def article(self):
+        self.current = "article"
 
-    def edit_screen(self):
-        self.current = "edit"
+    def corrector(self):
+        self.current = "corrector"
+
+    def back(self):
+        self.transition = SlideTransition(direction="right")
+        self.main()
+        self.transition = SlideTransition(direction="left")
 
 
 class Evento(MDApp):
 
+    def __init__(self):
+
+        super().__init__()
+        self.manager = ScreenManagement()
+
     def build(self):
-        Builder.load_file("view/management.kv")
-        self.root = ScreenManagement()
-        return self.root
+
+        main = MainScreen(name="main")
+        author = CorrectorScreen(name="corrector")
+        article = ArticleScreen(name="article")
+
+        self.manager.add_widget(main)
+        self.manager.add_widget(article)
+        self.manager.add_widget(author)
+        self.manager.main()
+
+        return self.manager
 
 
 if __name__ == '__main__':
