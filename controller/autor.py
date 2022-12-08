08f034d961e4b9cf0fa779f1ef881d9d388f334b
Jacob Costa <jacob.costa@estudante.ifro.edu.br>
2022-12-07 21:15:26 -0400

    Update autor.py
    Incluído os métodos select, select_id. Corrigido detalhes.

--- controller/autor.py
+++ controller/autor.py
@@ -2,8 +2,39 @@ import model.models as models
 
 
 class Autor:
-
-    def salvar(self, id=None, email=None, nome=None, instituicao=None):
+        
+    def select(self):
+        autores = []
+        try:
+            select = models.Autor.select()
+            for autor in select:
+                data = {"id": autor.id,
+                        "email": autor.email,
+                        "nome": models.Participante.get_by_id(autor.id).nome,
+                        "instituicao": models.Participante.get_by_id(autor.id).instituicao}
+                autores.append(data)
+            return autores
+        except Exception as e:
+            print(e)
+            return False
+        
+                        
+    def select_id(self,id):
+        try:
+            autor = models.Autor.get_by_id(id)
+            participante = models.Participante.get_by_id(id)
+            
+            data = {"id": autor.id,
+                    "email": autor.email,
+                    "nome": participante.nome,
+                    "instituicao": participante.instituicao}
+            return data
+        except Exception as e:
+            print(e)
+            return False
+                
+    
+    def save(self, id=None, email=None, nome=None, instituicao=None):
         try:
             if id:
                 autor = models.Autor.get_by_id(id)
@@ -15,12 +46,14 @@ class Autor:
                 autor = models.Autor(email=email)
                 participante = models.Participante(nome=nome, instituicao=instituicao)
             autor.save()
+            participante.save()
             return True
         except Exception as e:
             print(e)
             return False
-
-    def excluir(self, id):
+    
+    
+    def delete(self, id):
         try:
             autor = models.Autor.get_by_id(id)
             autor.delete_instance()
